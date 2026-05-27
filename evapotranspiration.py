70b1b4412e5a37622ca8892be3b625a6475ac85f
c.rohel_7 <shrestha@campus.tu-berlin.de>
2026-05-27 14:13:43 +0200

    Implement evapotranspiration model

--- evapotranspiration.py
+++ evapotranspiration.py
@@ -3,9 +3,14 @@
 # checkout main class and sub class
 #gamma is constal 
 
-def evapotranspiration(crop_coefficient: float,
-                        reference_evapotranspiration: float)-> float:
-    ET = crop_coefficient * reference_evapotransiration
+def water_volume_loss_to_evaporation()-> float:
+    
+    pass
+
+def crop_evapotranspiration(reference_evapotranspiration: float,
+                            balcony_microclimate_correction: float = 1.0, 
+                            crop_coefficient: float = 1.0)-> float:
+    ET = crop_coefficient * balcony_microclimate_correction * reference_evapotranspiration
     return ET
 
 def reference_evapotransiration(slope_vapour_pressure: float, 
@@ -14,10 +19,11 @@ def reference_evapotransiration(slope_vapour_pressure: float,
                                 air_temperature: float,
                                 wind_speed: float,
                                 saturation_vapour_pressure: float,
-                                actual_vapout_pressure: float)-> float:
-    gamma = 0.5
-    ref_ET = 0.408 * slope_vapour_pressure  * (net_radiation - soil_heat_flux_density) \
+                                actual_vapout_pressure: float,
+                                gamma=0.5)-> float:
+    numerator = 0.408 * slope_vapour_pressure  * (net_radiation - soil_heat_flux_density) \
         + gamma * (900 / (air_temperature + 273)) * wind_speed *\
-        (saturation_vapour_pressure - actual_vapout_pressure)/ slope_vapour_pressure + \
-        gamma * (1 + (0.34 * wind_speed))
+        (saturation_vapour_pressure - actual_vapout_pressure)
+    denominator = slope_vapour_pressure + gamma * (1 + (0.34 * wind_speed))
+    ref_ET = numerator / denominator
     return ref_ET
